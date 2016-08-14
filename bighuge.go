b57c840c8ebc44914513fc225fd7c9f551a51043
ken5scal <kengoscal@gmail.com>
2016-08-14 20:27:06 +0900

    Completed bighuge.go

--- bighuge.go
+++ bighuge.go
@@ -1,4 +1,10 @@
-package main
+package thesaurus
+
+import (
+	"net/http"
+	"fmt"
+	"encoding/json"
+)
 
 type BigHuge struct {
 	APIKEy string
@@ -13,7 +19,23 @@ type words struct {
 	Syn []string `json:"syn"`
 }
 
+func (b *BigHuge) Synonyms(term string) ([]string, error) {
+	var syns []string
+	response, err := http.Get("http://words.bighugelabs.com/api/2/" +
+		b.APIKEy +"/" + term + "/json")
 
+	if err != nil {
+		return syns, fmt.Errorf("bighuge: Failed searching synonyms %q: %v", term, err)
+	}
 
-func main() {
+	var data synonyms
+	defer response.Body.Close()
+
+	if err := json.NewDecoder(response.Body).Decode(&data); err != nil {
+		return syns, err
+	}
+	syns = append(syns, data.Noun.Syn...)
+	syns = append(syns, data.Verb.Syn...)
+
+	return syns, nil
 }
